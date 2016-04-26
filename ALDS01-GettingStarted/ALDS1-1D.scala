ea1eaf79b07c89761910dd43375a18f7d408ab5c
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-04-26 20:03:00 +0900

    WIP 1-1D

--- ALDS01-GettingStarted/ALDS1-1D.scala
+++ ALDS01-GettingStarted/ALDS1-1D.scala
@@ -1,4 +1,5 @@
 import scala.io.Source
+import scala.annotation.tailrec
 
 object Main{
   def main(args: Array[String]){
@@ -9,6 +10,7 @@ object Main{
     println(rec(lines.tail, Int.MinValue))
   }
 
+  @tailrec
   def rec(list: List[Int], max: Int): Int ={
     if(list.length == 1) max
     else rec(list.init, Math.max(list.last - list.init.min, max))
