cdad0dfefd7182905a9db6af110872cb0fd70c68
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-04-30 02:57:28 +0900

    Modify 1-2B

--- ALDS02-SortI/ALDS1-2B.scala
+++ ALDS02-SortI/ALDS1-2B.scala
@@ -17,17 +17,10 @@ object Main{
     else if(unsorted.sorted == unsorted) n
     else {
       val pos = unsorted.indexWhere(_ == unsorted.min)
-      // unsortedのうち一番小さい値と0番目を交換、0番目をsortedへ追加、unsortedから0番目を削除
       val swapped = unsorted.updated(0, unsorted(pos)).updated(pos, unsorted(0))
 
-      if(pos == 0) sSort(sorted :+ unsorted.min, deleteOne(swapped, 0), n)
-      else sSort(sorted :+ unsorted.min, deleteOne(swapped, 0), n+1)
+      if(pos == 0) sSort(sorted :+ unsorted.min, swapped.tail, n)
+      else sSort(sorted :+ unsorted.min, swapped.tail, n+1)
     }
-  }
-
-  def deleteOne(list: List[Int], index: Int): List[Int] = {
-    val buf = ListBuffer(list: _*)
-    buf.remove(index)
-    buf.toList
   }
 }
