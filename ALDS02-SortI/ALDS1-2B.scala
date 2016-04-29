54793a44fb8844b22e7445dcb20e67f2815bdb21
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-04-30 03:33:16 +0900

    Modify 1-1B faster

--- ALDS02-SortI/ALDS1-2B.scala
+++ ALDS02-SortI/ALDS1-2B.scala
@@ -6,15 +6,14 @@ object Main{
     StdIn.readLine()
     val line = StdIn.readLine().split(" ").toList.map(_.toInt)
 
-    println(line.sorted.mkString(" "))
-    println(sSort(List(), line, 0))
+    val res = sSort(List(), line, 0)
+    println(res._1.mkString(" ") + "\n" + res._2)
   }
 
   @tailrec
-  def sSort(sorted: List[Int], unsorted: List[Int], n: Int): Int={
-    if(unsorted.length == 0) n
-    else if(unsorted.sorted == unsorted) n
-    else if(unsorted.indexWhere(_ == unsorted.min) == 0) sSort(sorted :+ unsorted.min, unsorted.tail, n)
+  def sSort(sorted: List[Int], unsorted: List[Int], n: Int):(List[Int], Int)={
+    if(unsorted.sorted == unsorted) ((sorted ::: unsorted), n)
+    else if(unsorted.indexWhere(_ == unsorted.min) == 0) sSort(sorted :+ unsorted.head, unsorted.tail, n)
     else {
       val pos = unsorted.indexWhere(_ == unsorted.min)
       val swapped = unsorted.updated(0, unsorted(pos)).updated(pos, unsorted.head)
