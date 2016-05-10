43392240922c33e37cb191698c8ceace6cabe4f5
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-05-10 23:21:10 +0900

    WIP

--- ALDS02-SortI/ALDS1-2A.scala
+++ ALDS02-SortI/ALDS1-2A.scala
@@ -6,21 +6,34 @@ object Main{
     StdIn.readLine()
     val line = StdIn.readLine().split(" ").toList.map(_.toInt)
 
-    val res = bSort(line)
+    val res = bSort(List(), list, 0)
     println(res._2.mkString(" ") + "\n" + res._1)
   }
 
-  def bSort(list: List[Int]):Int= bSortRec(List(), list, 0)
+  @tailrec
+  def bSort(sorted: List[Int], unsorted: List[Int], n: Int): Tuple2[Int, List[Int]] ={
+    if(unsorted.sorted == unsorted) ((sorted ::: unsorted), n)
+    else if(unsorted.indexWhere(_ == unsorted.min) == 0) bSort(sorted :+ unsorted.head, unsorted.tail, n)
+    else{
+      val swapnum = sortRec(unsorted, 0)
+
+      unsorted.foldRight(List(Int.MaxValue)){ (a, b) =>
+        if(a < b.head) a :: b
+        else b.head :: a :: b.tail
+      }
+
+      bSort(sorted :+ sort1th.head, sort1th.tail, n + swapnum)
+    }
+  }
 
   @tailrec
-  def bSortRec(sorted: List[Int], unsorted: List[Int], n: Int): Tuple2[Int, List[Int]] ={
-    if(unsorted.length == 0) (n, sorted)
-    else{
-      //unsortedの配列をbubbleソートする
-      val bubble = unsorted.foldRight(Int.MaxValue){(a, b) => if(a < b) List(a, b) else b}
-
-      //unsortedのheadをsorted、tailをunsortedとして再帰
-
+  def sortRec(list: List[Int], n: Int): Int ={
+    if() n
+    else {
+      list.foldRight(List(Int.MaxValue)){ (a, b) =>
+        if(a < b.head) a :: b
+        else b.head :: a :: b.tail
+      }
     }
   }
 }
